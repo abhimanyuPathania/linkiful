effb2ee380cad8c308c961f0e72f72baad612f11
abhimanyuPathania <abpindia1944@gmail.com>
2015-02-07 15:57:51 +0530

    TEST basic css transitions FIX backup variable name bug

--- backup.js
+++ backup.js
@@ -1,9 +1,9 @@
 
 var result = document.querySelector("#result");
 
-if (localStorage.getItem("linkman")) {
-	var backupText = document.createTextNode(localStorage.getItem("linkman"));
+if (localStorage.getItem("linkiful")) {
+	var backupText = document.createTextNode(localStorage.getItem("linkiful"));
 	result.appendChild(backupText);
 } else {
-	result.innerHTML = "<img src='http://i.memeful.com/media/post/PdW9kOM_700wa_0.gif'></img>"
+	result.innerHTML = "<img src='http://i.memeful.com/media/post/PdW9kOM_700wa_0.gif' alt='ahahaha NO!'></img>"
 }
